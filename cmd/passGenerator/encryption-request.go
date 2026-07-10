150d23228ad7a98dad8e9ea6b520a1c3b1651bd1
Teodor Varbanov <tvarbanov@outlook.com>
2026-07-10 12:13:14 +0300

    Added logic for connecting to the mock server, sending the request and reading the enc pass as response

---
+++ cmd/passGenerator/encryption-request.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"time"
+)
+
+type passEncrypt struct {
+	Password string `json:"password"` //here it should probably be a marshalled passwod taken from the pass generator
+}
+
+func encRequest(customClient *http.Client, secret string, apiKey string) (*http.Response, error) {
+	pass := passEncrypt{
+		Password: secret,
+	}
+
+	jsonData, err := json.Marshal(pass)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", "http://localhost:6969/rest/api/latest/encrypt", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+apiKey)
+	req.Header.Set("User-Agent", "Go-HTTP-Client/1.0")
+
+	return customClient.Do(req)
+}
+
+func customClient() *http.Client {
+	transport := &http.Transport{
+		MaxIdleConns:        100,
+		MaxIdleConnsPerHost: 10,
+		IdleConnTimeout:     90 * time.Second,
+		DisableKeepAlives:   false,
+		DisableCompression:  false,
+	}
+
+	return &http.Client{
+		Transport: transport,
+		Timeout:   30 * time.Second,
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			if len(via) >= 10 {
+				return errors.New("Stopped after 10 redirects.")
+			}
+			return nil
+		},
+	}
+
+}
--- cmd/passGenerator/main.go
+++ cmd/passGenerator/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
+//the implemented mock api server expects the following: curl -X POST http://localhost:6969/rest/api/latest/encrypt -H "Authorization: Bearer very-secret-token"  -H "Content-Type: application/json"   -d '{"password":"mySecret123"}
 //to-do change the contants to paramters given at command line or via a parameter file
 //to-do implement logic to exclude certain numbers based on acii table
 //to-do implement logic to exclude numbers based on provided input
@@ -12,10 +15,24 @@ import (
 const ASCII_BEGIN = 33
 const ASCII_END = 126
 const LENGHT = 16
+const apiKey = "very-secret-token"
 
 func main() {
 	var exclusions []string
 	exclusions = []string{"&", "$", ":", "*", "}", "^", "@"} //example exclusions - to be parameterized
 
-	fmt.Println(generatePassword(LENGHT, exclusions))
+	secret := generatePassword(LENGHT, exclusions)
+
+	res, err := encRequest(customClient(), secret, apiKey)
+	if err != nil {
+		fmt.Printf("Could not make a request: %s\n", err)
+		os.Exit(1)
+	}
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("client: could not read response body: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%s\n", resBody)
+
 }
